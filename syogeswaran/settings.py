94c037d244c8c23f6b4db7d371569eb51d19868e
sajeevany <sajeevanyogeswaran@gmail.com>
2016-08-01 19:00:19 -0500

    Changes to add TesseractBoxEditor module

--- syogeswaran/settings.py
+++ syogeswaran/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'syogesweb',
-    'tesseractboxeditor',
+    'TesseractBoxEditor',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- syogeswaran/urls.py
+++ syogeswaran/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^apps/tbe/', include('TesseractBoxEditor.urls')),
     url(r'^', include('syogesweb.urls')),
-    url(r'^apps/tbe/', include('tesseractboxeditor.urls')),
 ]
